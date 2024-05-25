02f5726cbcc6d7fb7ee615de649a1da2f0ba0921
OmarThierno <thiernomarndiaye76@gmail.com>
2024-05-25 18:15:55 +0200

    feat: done bonus 1

--- js/script.js
+++ js/script.js
@@ -4,11 +4,34 @@ createApp({
   data() {
     return{
       diskArray: [],
+      like: 'like'
+      
     }
   },
   created() {
-    axios.get("http://localhost:8888/boolean/php-dischi-json/server.php").then((resp) => {
+    this.getAllDisc()
+  },
+  methods: {
+    getAllDisc() {
+      axios.get("http://localhost:8888/boolean/php-dischi-json/server.php").then((resp) => {
       this.diskArray = resp.data.results;
     })
+    },
+    toogleLike(curIndex){
+      const data = {
+        like: this.like,
+        index: curIndex,
+      }
+      console.log(data);
+
+      axios.post("http://localhost:8888/boolean/php-dischi-json/server.php", data, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      }).then((resp) => {
+        this.diskArray = resp.data.results;
+        console.log(resp.data.results)
+      })
+    },
   }
 }).mount('#app')
--- server.php
+++ server.php
@@ -3,6 +3,21 @@ $list_disk = file_get_contents("dischi.json");
 
 
 $list = json_decode($list_disk, true);
+// if (!isset($list[0]["like"])) {
+//   for ($i = 0; $i < count($list); $i++) {
+//     $list[$i]['like'] = false;
+//   }
+// }
+
+if (isset($_POST["like"])) {
+
+  $index = $_POST["index"];
+  $like = $_POST["like"];
+  if ($like === 'like') {
+    $list[$index]["like"] = !$list[$index]["like"];
+    file_put_contents("dischi.json", json_encode($list));
+  }
+}
 
 $response_date = [
   "results" => $list,
